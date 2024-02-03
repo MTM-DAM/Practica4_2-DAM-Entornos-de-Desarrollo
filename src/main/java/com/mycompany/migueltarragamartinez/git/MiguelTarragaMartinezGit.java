6929bc94f21f4e46435857bdff030834c33e043b
Miguel <Miguel@DESKTOP-O23B0P5>
2024-02-03 13:36:00 +0200

    "rama con suma de variables"

--- src/main/java/com/mycompany/migueltarragamartinez/git/MiguelTarragaMartinezGit.java
+++ src/main/java/com/mycompany/migueltarragamartinez/git/MiguelTarragaMartinezGit.java
@@ -9,8 +9,19 @@ package com.mycompany.migueltarragamartinez.git;
  * @author Miguel
  */
 public class MiguelTarragaMartinezGit {
+    
+   
 
     public static void main(String[] args) {
+        
+        int numero1 = 1;
+        int numero2 = 4;
+        int suma = numero1+numero2;
+        
         System.out.println("Practica4_2 Entornos de Desarrollo");
+        
+        System.out.println("La suma de numero1 y numero2 es: "+ suma);
+        
+        
     }
 }
