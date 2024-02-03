9b6c782b8941f04c173db99b2ff154248f76d8a7
Miguel <Miguel@DESKTOP-O23B0P5>
2024-02-03 17:51:51 +0200

    "modificiación de código master para provocar conflicto"

--- src/main/java/com/mycompany/migueltarragamartinez/git/MiguelTarragaMartinezGit.java
+++ src/main/java/com/mycompany/migueltarragamartinez/git/MiguelTarragaMartinezGit.java
@@ -9,8 +9,19 @@ package com.mycompany.migueltarragamartinez.git;
  * @author Miguel
  */
 public class MiguelTarragaMartinezGit {
+    
+   
 
     public static void main(String[] args) {
+        
+        int numero1 = 20;
+        int numero2 = 10;
+        int resta = numero1-numero2;
+        
         System.out.println("Practica4_2 Entornos de Desarrollo");
+        
+        System.out.println("La resta de numero1 y numero2 es: "+ resta);
+        
+        
     }
 }
